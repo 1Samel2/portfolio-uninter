c9b6fc9f0f4ac1616fa0cf860d2d3aac38207950
Samuel Guedes <91099488+1Samel2@users.noreply.github.com>
2022-08-22 23:40:04 -0300

    att

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const btn = document.getElementById("btnTop")
+const btn = document.getElementById("btnTop")  /* exportei o id da pasta html para usar aqui  */
 
-btn.addEventListener("click", function(){
+btn.addEventListener("click", function(){    /* um click para validar quando eu clicar no botão ao apertar a function roda o script  */
     window.scrollTo(0,0)
 })
 
-document.addEventListener('scroll',ocultar)
+document.addEventListener('scroll',ocultar)  /* addEventListener() registra uma única espera de evento em um único alvo */
 
 function ocultar(){
-    if(window.scrollY > 10){
+    if(window.scrollY > 10){     /*   if para validar se está no local que eu quero aí ativar o efeito  */
         btn.style.display = "flex"
     } else {
-        btn.style.display = "none"
+        btn.style.display = "none"     /* caso dá else ele não valida e deixa um display none para o css não exibir o conteúdo na tela  */
     }
 }
 
