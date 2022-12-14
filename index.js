913d3fb522e5863c32b6591c505bbf3f8d25f83d
samuel <samuelsguedes@hotmail.com>
2022-12-14 12:00:28 -0300

    Alterando layout

--- index.js
+++ index.js
@@ -15,3 +15,12 @@ function ocultar(){
 }
 
 ocultar()
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
