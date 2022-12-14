c9f7c1fff5f3b369987cd52e653c814c7a10883d
samuel <samuelsguedes@hotmail.com>
2022-12-14 12:54:25 -0300

    att

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
 const btn = document.getElementById("btnTop")  /* exportei o id da pasta html para usar aqui  */
 
-btn.addEventListener("click", function(){    /* um click para validar quando eu clicar no botão ao apertar a function roda o script  */
-    window.scrollTo(0,0)
+btn.addEventListener("click", function () {    /* um click para validar quando eu clicar no botão ao apertar a function roda o script  */
+    window.scrollTo(0, 0)
 })
 
-document.addEventListener('scroll',ocultar)  /* addEventListener() registra uma única espera de evento em um único alvo */
+document.addEventListener('scroll', ocultar)  /* addEventListener() registra uma única espera de evento em um único alvo */
 
-function ocultar(){
-    if(window.scrollY > 10){     /*   if para validar se está no local que eu quero aí ativar o efeito  */
+function ocultar() {
+    if (window.scrollY > 10) {     /*   if para validar se está no local que eu quero aí ativar o efeito  */
         btn.style.display = "flex"
     } else {
         btn.style.display = "none"     /* caso dá else ele não valida e deixa um display none para o css não exibir o conteúdo na tela  */
@@ -16,11 +25,3 @@ function ocultar(){
 
 ocultar()
 
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
